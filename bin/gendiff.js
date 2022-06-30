067bae737da5bf3e4f30c3d1378a642c4390d8d7
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-06-30 13:53:53 +0300

    updated bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,10 @@ const { program } = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1>')
+  .arguments('<filepath2>');
 
 program.parse();
 
