8d25d202dd351e7336a11f8a7150ab3b569f4029
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-07-14 15:29:20 +0300

    refactored src/diff.js

--- src/diff.js
+++ src/diff.js
@@ -1,61 +1,30 @@
 import _ from 'lodash';
 
-const diff = (file1, file2) => {
-  const iter = (obj1, obj2) => {
-    const totalKeys = _.sortedUniq([...Object.keys(obj1), ...Object.keys(obj2)].sort());
+const diff = (obj1, obj2) => {
+  if ((!_.isPlainObject(obj1) && !_.isPlainObject(obj2))
+    || (!_.isPlainObject(obj1) && Object.keys(obj2).length === 0)
+  ) return obj1;
+  const totalKeys = _.sortedUniq([...Object.keys(obj1), ...Object.keys(obj2)].sort());
 
-    return totalKeys.map((key) => {
-      if (Object.keys(obj2).length === 0) {
-        if (_.isPlainObject(obj1[key])) {
-          return { status: 'unchanged', node: key, value: iter(obj1[key], {}) };
-        }
-        return { status: 'unchanged', node: key, value: obj1[key] };
+  return totalKeys.map((key) => {
+    if (Object.keys(obj2).length === 0 || Object.keys(obj1).length === 0) {
+      return { status: 'unchanged', node: key, value: diff(obj1[key] || obj2[key], {}) };
+    }
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      if ((obj1[key] === obj2[key])
+        || (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key]))
+      ) {
+        return { status: 'unchanged', node: key, value: diff(obj1[key], obj2[key] || {}) };
       }
-
-      if (Object.keys(obj1).length === 0) {
-        if (_.isPlainObject(obj2[key])) {
-          return { status: 'unchanged', node: key, value: iter(obj2[key], {}) };
-        }
-        return { status: 'unchanged', node: key, value: obj2[key] };
-      }
-
-      if (_.has(obj1, key) && _.has(obj2, key)) {
-        if ((obj1[key] === obj2[key])) {
-          return { status: 'unchanged', node: key, value: obj1[key] };
-        }
-        if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-          return { status: 'unchanged', node: key, value: iter(obj1[key], obj2[key]) };
-        }
-        if (_.isPlainObject(obj1[key])) {
-          return [
-            { status: 'removed', node: key, value: iter(obj1[key], {}) },
-            { status: 'added', node: key, value: obj2[key] },
-          ];
-        }
-        if (_.isPlainObject(obj2[key])) {
-          return [
-            { status: 'removed', node: key, value: obj1[key] },
-            { status: 'added', node: key, value: iter(obj2[key], {}) },
-          ];
-        }
-        return [
-          { status: 'removed', node: key, value: obj1[key] },
-          { status: 'added', node: key, value: obj2[key] },
-        ];
-      }
-      if (_.has(obj1, key)) {
-        if (_.isPlainObject(obj1[key])) {
-          return { status: 'removed', node: key, value: iter(obj1[key], {}) };
-        }
-        return { status: 'removed', node: key, value: obj1[key] };
-      }
-      if (_.isPlainObject(obj2[key])) {
-        return { status: 'added', node: key, value: iter(obj2[key], {}) };
-      }
-      return { status: 'added', node: key, value: obj2[key] };
-    }).flat();
-  };
-  return iter(file1, file2);
+      return [
+        { status: 'removed', node: key, value: diff(obj1[key], {}) },
+        { status: 'added', node: key, value: diff(obj2[key], {}) },
+      ];
+    }
+    return _.has(obj1, key)
+      ? { status: 'removed', node: key, value: diff(obj1[key], {}) }
+      : { status: 'added', node: key, value: diff(obj2[key], {}) };
+  }).flat();
 };
 
 export default diff;
