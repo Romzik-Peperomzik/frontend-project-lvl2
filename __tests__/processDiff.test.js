012a86563e6e601eee983599acb25cb41f7d96f6
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-07-15 09:14:52 +0300

    updated files in __fixtures__/, added processDiffFilesPlainStyle test in __tests__/processDiff.test.js, updated filenames in __test__/processDiff.test.js

--- __tests__/processDiff.test.js
+++ __tests__/processDiff.test.js
@@ -2,19 +2,21 @@ import { expect, test, beforeAll } from '@jest/globals';
 import processDiffFiles from '../src/processDiff.js';
 import { getFixturePath, readFile } from '../src/readFiles.js';
 
-let expectedAnswer;
+let expectedAnswerDefaultStyle;
+let expectedAnswerPlainStyle;
 let jsonFilePath1;
 let jsonFilePath2;
 let yamlFilePath1;
 let yamlFilePath2;
 
 beforeAll(() => {
-  expectedAnswer = readFile(getFixturePath('processDiff.nested.expected.txt'));
+  expectedAnswerDefaultStyle = readFile(getFixturePath('processDiff.stylish.expected.txt'));
+  expectedAnswerPlainStyle = readFile(getFixturePath('processDiff.plain.expected.txt'));
   const fileNames = [
-    'file1_nested.json',
-    'file2_nested.json',
-    'file1_nested.yaml',
-    'file2_nested.yaml',
+    'file1.json',
+    'file2.json',
+    'file1.yaml',
+    'file2.yaml',
   ];
   [
     jsonFilePath1,
@@ -24,7 +26,12 @@ beforeAll(() => {
   ] = fileNames.map((fileName) => getFixturePath(fileName));
 });
 
-test('processDiffFiles', () => {
-  expect(processDiffFiles(jsonFilePath1, jsonFilePath2)).toEqual(expectedAnswer);
-  expect(processDiffFiles(yamlFilePath1, yamlFilePath2)).toEqual(expectedAnswer);
+test('processDiffFilesDefaultStyle', () => {
+  expect(processDiffFiles(jsonFilePath1, jsonFilePath2)).toEqual(expectedAnswerDefaultStyle);
+  expect(processDiffFiles(yamlFilePath1, yamlFilePath2)).toEqual(expectedAnswerDefaultStyle);
+});
+
+test('processDiffFilesPlainStyle', () => {
+  expect(processDiffFiles(jsonFilePath1, jsonFilePath2, 'plain')).toEqual(expectedAnswerPlainStyle);
+  expect(processDiffFiles(yamlFilePath1, yamlFilePath2, 'plain')).toEqual(expectedAnswerPlainStyle);
 });
