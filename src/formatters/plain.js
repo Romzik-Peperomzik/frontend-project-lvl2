4332b752c6aed3197230468c4fa7a99b802aab82
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-07-18 01:46:05 +0300

    refactored src/formatters/plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,34 +1,48 @@
 import _ from 'lodash';
 
-const processNode = (obj, ancestry) => {
-  const [nodeName, status, value] = [obj.node, obj.status, obj.value];
-  const complexValue = _.isArray(value) ? '[complex value]' : value;
-  if (obj.status === 'updated') {
-    const complexNewValue = _.isArray(obj.newValue) ? '[complex value]' : obj.newValue;
-    return {
-      [nodeName]: {
-        ancestry, newValue: complexNewValue, status, value: complexValue,
-      },
-    };
+const specifyValue = (value) => {
+  switch (value) {
+    case true:
+    case false:
+    case null:
+    case '[complex value]':
+      return true;
+    default:
+      return false;
   }
-  return { [nodeName]: { ancestry, status, value: complexValue } };
+};
+
+const processNode = (obj, ancestry) => {
+  const complexValue = _.isArray(obj.value) ? '[complex value]' : obj.value;
+  const newComplexValue = _.isArray(obj.newValue) ? '[complex value]' : obj.newValue;
+  const oldValue = specifyValue(complexValue) ? complexValue : `'${complexValue}'`;
+  const newValue = specifyValue(newComplexValue) ? newComplexValue : `'${newComplexValue}'`;
+
+  switch (obj.status) {
+    case 'updated':
+      return `Property '${ancestry}' was updated. From ${oldValue} to ${newValue}`;
+    case 'added':
+      return `Property '${ancestry}' was added with value: ${oldValue}`;
+    case 'removed':
+      return `Property '${ancestry}' was removed`;
+    default:
+      return '';
+  }
 };
 
 const plain = (arrayWithObjectsForFormatting) => {
   const iter = (obj, ancestry = '') => {
-    const updAncestry = ancestry ? `${ancestry}.${obj.node}` : obj.node;
-    const currentNode = processNode(obj, updAncestry);
-    if (!_.isArray(obj.value)) {
-      return currentNode;
+    if (!_.isArray(obj)) {
+      const updAncestry = ancestry ? `${ancestry}.${obj.node}` : obj.node;
+      const currentNode = processNode(obj, updAncestry);
+      if (!_.isArray(obj.value)) {
+        return currentNode;
+      }
+      return [currentNode, ...iter(obj.value, updAncestry)].filter((item) => item).join('\n');
     }
-    const valuesOfCurrentNode = obj.value.reduce((acc, node) => {
-      const processedNodes = iter(node, updAncestry);
-      return { ...acc, ...processedNodes };
-    }, {});
-    return { ...currentNode, ...valuesOfCurrentNode };
+    return obj.map((item) => iter(item, ancestry));
   };
-  const result = arrayWithObjectsForFormatting.reduce((acc, obj) => ({ ...acc, ...iter(obj) }), {});
-  return result;
+  return iter(arrayWithObjectsForFormatting).join('\n');
 };
 
 export default plain;
