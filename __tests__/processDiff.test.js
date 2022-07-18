d255b2240605f1bbf893a1e56a8c440e347d7a86
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-07-19 00:04:36 +0300

    refactored processDiff.test.js

--- __tests__/processDiff.test.js
+++ __tests__/processDiff.test.js
@@ -1,32 +1,22 @@
-import { expect, test, beforeAll } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 import processDiffFiles from '../src/processDiff.js';
 import { getFixturePath, readFile } from '../src/readFiles.js';
 
-let expectedAnswerDefaultStyle;
-let expectedAnswerPlainStyle;
-let expectedAnswerJSONStyle;
-let jsonFilePath1;
-let jsonFilePath2;
-let yamlFilePath1;
-let yamlFilePath2;
-
-beforeAll(() => {
-  expectedAnswerDefaultStyle = readFile(getFixturePath('processDiff.stylish.expected.txt'));
-  expectedAnswerPlainStyle = readFile(getFixturePath('processDiff.plain.expected.txt'));
-  expectedAnswerJSONStyle = readFile(getFixturePath('processDiff.json.expected.txt'));
-  const fileNames = [
-    'file1.json',
-    'file2.json',
-    'file1.yaml',
-    'file2.yaml',
-  ];
-  [
-    jsonFilePath1,
-    jsonFilePath2,
-    yamlFilePath1,
-    yamlFilePath2,
-  ] = fileNames.map((fileName) => getFixturePath(fileName));
-});
+const expectedAnswerDefaultStyle = readFile(getFixturePath('processDiff.stylish.expected.txt'));
+const expectedAnswerPlainStyle = readFile(getFixturePath('processDiff.plain.expected.txt'));
+const expectedAnswerJSONStyle = readFile(getFixturePath('processDiff.json.expected.txt'));
+const fileNames = [
+  'file1.json',
+  'file2.json',
+  'file1.yaml',
+  'file2.yaml',
+];
+const [
+  jsonFilePath1,
+  jsonFilePath2,
+  yamlFilePath1,
+  yamlFilePath2,
+] = fileNames.map((fileName) => getFixturePath(fileName));
 
 test('processDiffFilesDefaultStyle', () => {
   expect(processDiffFiles(jsonFilePath1, jsonFilePath2)).toEqual(expectedAnswerDefaultStyle);
