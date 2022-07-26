c5932bcc4a1a699fc8c4cc42fe4cf21bdc5ab8b2
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-07-26 07:03:49 +0300

    moved src/readFiles.js-readFileAndType() to processDiff.js

--- src/processDiff.js
+++ src/processDiff.js
@@ -1,7 +1,14 @@
+import fs from 'fs';
+import { extname } from 'node:path';
 import formatter from './formatters/index.js';
 import parseFile from './parsers.js';
 import makeAST from './processAST.js';
-import readFileAndType from './readFiles.js';
+
+const readFileAndType = (filepath) => {
+  const file = fs.readFileSync(filepath, 'utf-8');
+  const type = extname(filepath);
+  return [file, type];
+};
 
 const genDiff = (filePath1, filePath2, formatType = 'stylish') => {
   const dataWithTypes = [filePath1, filePath2].map((fp) => readFileAndType(fp));
