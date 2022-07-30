d5479b12eee5893808baaf6390f8f7818f48218c
Romzik-Peperomzik <warriorway5657@gmail.com>
2022-07-30 14:54:32 +0300

    refactored src/formatters/plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -6,32 +6,27 @@ const specifyValue = (value) => {
   return value;
 };
 
-const processNode = (obj, ancestry) => {
+const plain = (obj, ancestry = '') => {
+  const updAncestry = ancestry ? `${ancestry}.${obj.node}` : obj.node;
   const [value, value1] = [specifyValue(obj.value), specifyValue(obj.value1)];
 
   switch (obj.status) {
     case 'updated':
-      return `Property '${ancestry}' was updated. From ${value} to ${value1}`;
+      return `Property '${updAncestry}' was updated. From ${value} to ${value1}`;
     case 'added':
-      return `Property '${ancestry}' was added with value: ${value}`;
+      return `Property '${updAncestry}' was added with value: ${value}`;
     case 'removed':
-      return `Property '${ancestry}' was removed`;
+      return `Property '${updAncestry}' was removed`;
     case 'nested':
+      return obj.children
+        .map((child) => (plain(child, updAncestry)))
+        .filter((item) => item)
+        .join('\n');
     case 'unchanged':
       return undefined;
     default:
       throw new Error(`unknown status: ${obj.status}`);
   }
-};
-
-const plain = (obj, ancestry = '') => {
-  const updAncestry = ancestry ? `${ancestry}.${obj.node}` : obj.node;
-  const currentNode = processNode(obj, updAncestry);
-  if (!obj.children) {
-    return currentNode;
-  }
-  const children = obj.children.map((child) => (plain(child, updAncestry))).filter((item) => item).join('\n');
-  return [currentNode, children].filter((item) => item).join('\n');
 };
 
 export default (processedAST) => processedAST.map((item) => plain(item)).join('\n');
